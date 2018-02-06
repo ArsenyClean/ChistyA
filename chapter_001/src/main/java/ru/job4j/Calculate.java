7221bf732cf58a1bb71ed657d925c26f257c6676
ArsenyClean <sobaka_multic_147@mail.ru>
2018-02-06 23:47:23 +0300

    1.3 VavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**
+* Class Calculate решение задачи части 001 урок 1
+* @author Chisty Arseny
+* @since 06.02.2018
+*/
+
+
 public class Calculate {
+
+	/**
+	* Конструктор, вывод строки в консоль.
+	* @param arg -arg
+	*/
 
 	public static void main(String [] args){
 		System.out.println("Hello world!");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for Calculate task
+* @author Chisty Arseny
+* @version 1.0
+* @since 06.02.2018
+*/
+
+package ru.job4j;
