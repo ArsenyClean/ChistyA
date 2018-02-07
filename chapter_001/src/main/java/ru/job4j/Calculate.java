56b85cca5bef9f71585460719e96b04c8eced587
ArsenyClean <sobaka_multic_147@mail.ru>
2018-02-07 22:03:15 +0300

    check

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,30 +1,31 @@
 package ru.job4j;
 
 /**
-* Class Calculate решение задачи части 001 урок 1
-* @author Chisty Arseny
-* @since 07.02.2018
-*/
+ * Class Calculate решение задачи части 001 урок 1
+ * @author Chisty Arseny
+ * @since 07.02.2018
+ */
 
 
 public class Calculate {
 
 	/**
-	*	Method Echo
-	*	@param name Your name
-	*	@return Echo plus your name
-	*/
-	
-	public String echo(String name){
+	 *	Method Echo
+	 *	@param name Your name
+	 *	@return Echo plus your name
+	 */
+
+	public static String Echo(String name){
 		return "Echo, echo, echo:" + name;
 	}
-	
+
 	/**
-	* Конструктор, вывод результата echo в консоль.
-	* @param arg -arg
-	*/
+	 * Конструктор, вывод результата echo в консоль.
+	 * @param arg -arg
+	 */
 
 	public static void main(String [] args){
-		System.out.println(echo("Chisty Arseny"));
+
+		System.out.println(Echo("Chisty Arseny"));
 	}
 }
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,26 +7,26 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test
-*
-* Of class Calculate решение задачи части 001 урок 1
-* @author Chisty Arseny
-* @since 06.02.2018
-*/
+ * Test
+ *
+ * Of class Calculate решение задачи части 001 урок 1
+ * @author Chisty Arseny
+ * @since 06.02.2018
+ */
 
 
 public class CalculateTest {
 
 	/**
-	* Тест для метода main
-	* @Test
-	*/
-
+	 * Тест для метода main
+	 * @Test
+	 */
+	@Test
 	public void whenTakeNameThenTreeEchoPlusName () {
 		String input = "Chisty Arseny";
 		String expect = "Echo, echo, echo:Chisty Arseny";
 		Calculate calc = new Calculate();
-		String result = calc.echo(input);
+		String result = calc.Echo(input);
 		assertThat(result, is(expect));
 	}
 }
