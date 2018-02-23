524204f59c8eef3c6f878ecb4cd23e15fa0e3bec
ArsenyClean <sobaka_multic_147@mail.ru>
2018-02-23 14:00:14 +0300

    3. Реализовать коллекцию типа Set на базе хэш-таблицы [#998]

--- chapter_001/src/main/java/ru/job4j/service/HashSet.java
+++ chapter_001/src/main/java/ru/job4j/service/HashSet.java
@@ -1,24 +1,39 @@
 package ru.job4j.service;
 
+/**
+    Class HashSet     выполнение задния 3. Реализовать коллекцию типа Set на базе хэш-таблицы [#998]
+    @ author Chisty Arseny
+    @ since 23/02/2018
+ */
+
 public class HashSet<E> {
 
     Object[] users;
     int hashConst = 16;
     int tableFull = 0;
 
-    public HashSet(Object[] users){
+    /**
+     * Конструктор инициализирующий массив
+     * @param users
+     */
+    public HashSet(Object[] users) {
         this.users = users;
     }
 
+    /**
+     * Метод add, добавляющий элемент в хэш-таблицу
+     * @param e
+     * @return
+     */
     boolean add(E e) {
         int index = hashMake(e);
-        while (true){
-            if (index < users.length){
-                while (users[index] == null){
-                    users[index] = e;
+        while (true) {
+            if (index < users.length) {
+                while (users[index] == null) {
+                    users[index]= e;
                     tableFull++;
-                    double K = ((double)tableFull)/(double) users.length;
-                    if ( K > 0.75 ){
+                    double coeff= ((double)tableFull)/(double)users.length;
+                    if (coeff > 0.75) {
                         users = growTable();
                         break;
                     }
@@ -31,19 +46,28 @@ public class HashSet<E> {
         }
     }
 
-    Object[] growTable(){
+    /**
+     * Метод удваивающий хэш-таблицу
+     * @return
+     */
+    Object[] growTable() {
         Object[] usersCop = new Object[users.length*2];
-        for ( int i = 0; i < users.length; i++ ){
+        for ( int i = 0; i < users.length; i++ ) {
             usersCop[i] = users[i];
         }
         return usersCop;
     }
 
+    /**
+     * Метод, проверяющий, содержит ли хэш-таблица элемент е
+     * @param e
+     * @return
+     */
     boolean contains(E e) {
         int index = hashMake(e);
-        while (true){
-            if (index < users.length){
-                while (users[index] != null){
+        while (true) {
+            if (index < users.length) {
+                while (users[index] != null) {
                     if (users[index] == e) {
                         index++;
                         return true;
@@ -57,20 +81,25 @@ public class HashSet<E> {
         }
     }
 
+    /**
+     * Метод, удаляющий элемент е
+     * @param e
+     * @return
+     */
     boolean remove(E e) {
         int index = hashMake(e);
         int flagEnd = index;
         boolean endFound = false;
-        while (true){
-            if (index < users.length){
-                while (users[index] != null){
+        while (true) {
+            if (index < users.length) {
+                while (users[index] != null) {
                     if (users[index] == e) {
                         flagEnd++;
                         endFound = true;
                     }
                     index++;
                     if (index < users.length)
-                        if (users[index] != e){
+                        if (users[index] != e) {
                             flagEnd--;
                             break;
                         }
@@ -90,9 +119,12 @@ public class HashSet<E> {
         }
     }
 
-    int hashMake(E value){
+    /**
+     * Метод, вычисляющий хэш-функцию
+     * @param value
+     * @return
+     */
+    int hashMake(E value) {
         return (((Integer) value) * hashConst) % users.length;
     }
-
 }
-
