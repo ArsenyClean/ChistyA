51674dc2cc7b7c8209ff8284af60ca387ef983ad
ArsenyClean <sobaka_multic_147@mail.ru>
2018-02-25 21:06:27 +0300

    5.2.1. Реализовать SimpleArray<T> [#156][#41980]

--- chapter_001/src/main/java/ru/job4j/service/SimpleArray.java
+++ chapter_001/src/main/java/ru/job4j/service/SimpleArray.java
@@ -1,65 +1,44 @@
 package ru.job4j.service;
-
 import java.util.Iterator;
-
 /**
  * @author Chisty Arseny
  * @since 19.02.2018
  * @param <T>
  */
-public class SimpleArray <T> implements Iterable<T>{
-
+public class SimpleArray <T> implements Iterable<T> {
     Object[] objects;
     int index = 0;
-    int indexIterator = 0;
-
-    public SimpleArray(int size){
+    public SimpleArray(int size) {
         this.objects = new Object[size];
     }
-
-    public void add(T model){
-
+    public void add(T model) {
         this.objects[index] = model;
-        index++;
     }
-
-    public void set(int position, T model){
-
+    public void set(int position, T model) {
         this.objects[position] = model;
     }
-
     public void delete(int position) {
         int i = position + 1;
-        while (i < objects.length) {
-            objects[position] = objects[i];
+        while (objects[i] != null) {
+            objects[i] = objects[position];
             i++;
             position++;
-            index--;
         }
     }
-
-    public T get(int position){
-
+    public T get(int position) {
         return (T) this.objects[position];
     }
-
     @Override
     public Iterator<T> iterator() {
-
         return new Iterator<T>() {
             @Override
             public boolean hasNext() {
-                if (objects.length >= indexIterator )
-                    return true;
-                return false;
+                return index < objects.length;
             }
-
             @Override
             public T next() {
-                indexIterator++;
-                return (T) objects[indexIterator - 1];
+                return (T) objects[index];
             }
         };
     }
-
 }
