2022d0ebf41ef703d19132046250ce5c376ec79c
ArsenyClean <sobaka_multic_147@mail.ru>
2018-02-28 11:58:21 +0300

    Проверка

--- chapter_001/src/main/java/ru/job4j/service/Helper.java
+++ chapter_001/src/main/java/ru/job4j/service/Helper.java
@@ -4,16 +4,16 @@ package ru.job4j.service;
  * Helper class реализует методы с одинаковым функционалом для UserStore и RoleStore.
  * @author Chisty Arseny
  * @since 28.02.2018
- * @param <T>
+ * @param <T> может быть User или Role
  */
-public class Helper<T extends Base> {
+class Helper<T extends Base> {
     /**
      * seracher проводит поиск запрашиваемого элемента
-     * @param store
-     * @param id
+     * @param store присылаем наш массив
+     * @param id и искомый элемент
      * @return true если элемент найден, false если элемент не найден
      */
-    public int searcher(SimpleArray<T> store, String id) {
+    int searcher(SimpleArray<T> store, String id) {
         int i = 0;
         while (i < store.index) {
             if (store.objects[i].getId().equals(id)) {
@@ -27,12 +27,12 @@ public class Helper<T extends Base> {
 
     /**
      * shifter сдвигает исходный массив при удалении элемента
-     * @param store
-     * @param index
+     * @param store присылаем наш массив
+     * @param index и индекс искомого элемента
      * @return true, если сдвиг произведен false, если сдвиг не произведен (свиг не будет
      * произведен в случае, если удален последний элемент массива
      */
-    public boolean shifter(SimpleArray<T> store, int index) {
+    boolean shifter(SimpleArray<T> store, int index) {
         boolean doneFlag = false;
         int i = index;
         while (i < store.index - 1) {
@@ -46,14 +46,17 @@ public class Helper<T extends Base> {
 
     /**
      * toString переводит исходный массив в строку
-     * @param store
+     * @param store присылает обрабатываемый масси
      * @return строковый эквивален исходного массива
      */
-    public String toString(SimpleArray<T> store) {
-        String result = null;
+    String toString(SimpleArray<T> store) {
+        String result;
+        StringBuilder buffer = new StringBuilder();
         for (int i = 0; i < store.index; i++) {
-            result += "[" + i + "]=" + store.objects[i].getId() + " ";
+            result = "[" + i + "]=" + store.objects[i].getId() + " ";
+            buffer.append(result);
         }
+        result = buffer.toString();
         return result;
     }
 }
