6d160ff3ce4ff98ee9e1f1ac2058ce5e3fb012e5
ArsenyClean <sobaka_multic_147@mail.ru>
2018-03-02 14:00:12 +0300

    1. Создать модель User [#999][#42425]

---
+++ chapter_001/src/main/java/ru/job4j/mapservice/User.java
@@ -0,0 +1,14 @@
+package ru.job4j.mapservice;
+
+import java.util.Calendar;
+
+public class User {
+
+    public String name;
+    int children;
+    Calendar birthday;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
