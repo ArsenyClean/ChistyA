41e2f4e5ca36e572c98123ebc046ef413ff142f8
ArsenyClean <sobaka_multic_147@mail.ru>
2018-03-13 10:41:48 +0300

    2. Класс хранилища пользователей UserStorage [#1104]

---
+++ chapter_001/src/main/java/ru/job4j/currency/thread/userstorage/UserStorage.java
@@ -0,0 +1,77 @@
+package ru.job4j.currency.thread.userstorage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserStorage {
+
+    private List<User> list = new ArrayList<>();
+
+    public synchronized boolean add(User user) {
+        synchronized (this) {
+            User result = searchId(user.getId());
+            if (result == null) {
+                list.add(user);
+            }
+            return (result == null);
+        }
+    }
+
+    public synchronized boolean update(User user) {
+        synchronized (this) {
+            User result = searchId(user.getId());
+            if (result != null) {
+                result.setAmount(user.getAmount());
+            }
+            return (result != null);
+        }
+    }
+
+    public synchronized boolean delete(User user) {
+        synchronized (this) {
+            User result = searchId(user.getId());
+            if (result != null) {
+                list.remove(result);
+            }
+            return (result != null);
+        }
+    }
+
+    private synchronized User searchId(int id) {
+        User result = null;
+        for (User user : list) {
+            if (user.getId() == id) {
+                result = user;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        synchronized (this) {
+            User userFrom = searchId(fromId);
+            User userTo = searchId(toId);
+            boolean isDone = false;
+            if (userFrom != null && userTo != null) {
+                int operationRes = userFrom.getAmount() - amount;
+                if (operationRes >= 0) {
+                    userFrom.setAmount(operationRes);
+                    userTo.setAmount(operationRes + amount * 2);
+                    isDone = true;
+                }
+            }
+            return isDone;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            stringBuilder.append("[").append(list.get(i).getId()).append("]=").append(list.get(i).getAmount()).append(" ");
+        }
+        stringBuilder.append("\n");
+        return stringBuilder.toString();
+    }
+}
