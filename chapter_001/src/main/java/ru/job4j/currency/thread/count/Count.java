bceab7da3486f8d16a86fd3bf0825fd781bd1ed6
ArsenyClean <sobaka_multic_147@mail.ru>
2018-03-15 10:20:12 +0300

    1. класс Count и метод int incremant() [ #1102]

--- chapter_001/src/main/java/ru/job4j/currency/thread/count/Count.java
+++ chapter_001/src/main/java/ru/job4j/currency/thread/count/Count.java
@@ -6,13 +6,13 @@ import net.jcip.annotations.ThreadSafe;
 @ThreadSafe
 public class Count {
 
+    @GuardedBy("this")
     private int value;
 
     public synchronized void increment() {
         this.value++;
     }
 
-    @GuardedBy("this")
     public synchronized int get() {
         return this.value;
     }
